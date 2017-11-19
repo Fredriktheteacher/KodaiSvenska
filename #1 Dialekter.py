aadc6a9225447f0641e4e5f3eab2469af6cbb4d5
Fredrik Johansson <fredriktheteacher@gmail.com>
2017-11-19 22:33:49 +0100

    Create #1 Dialekter.py
    Skapade filen och la in en else-sats.

---
+++ #1 Dialekter.py
@@ -0,0 +1,6 @@
+##Enkelt program som kollar om man är bleking
+fras="Den däkan är riktigt duktig på att spela basket."
+if fras.find('däkan'):
+  print('Du är en äkta bleking.')
+else:
+  print('Du är förmodligen inte bleking.')
