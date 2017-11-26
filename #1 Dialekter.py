9874a75f85d0ea59241ebb61b08e4fa82e111e58
Fredrik Johansson <fredriktheteacher@gmail.com>
2017-11-26 18:24:27 +0100

    Update #1 Dialekter.py
    La till input så att det stämmer med klippet på tuben.
    La även till URL till klippet i kommentar

--- #1 Dialekter.py
+++ #1 Dialekter.py
@@ -1,5 +1,6 @@
 # Skapad av Frippe 2017
 # fabelmakaren.se
+# Se avsnittet på YouTube: https://youtu.be/pgVbCzYPPBU
 
 # Enkelt program som kollar om man är bleking
 fras="Den däkan är riktigt duktig på att spela basket."
@@ -8,10 +9,20 @@ if "däkan" in fras:
 else:
   print('Du är förmodligen inte bleking.')
 
+  
+# Samma program fast med inmatning från tangentbordet
+
+fras = input('Skriv in fras som ska kollas: ')
+if 'däka' in fras:
+  print('Frasen innehåller blekingska')
+else:
+  print('Frasen innehåller INTE blekingska')
+
+
 # Förslag till progression:
-# Kolla om en eller flera ord ingår i en fras
-fras='den däkan jobbar redigt hårt'
-ordlista=['däka','färas','redig']
+# Kolla om en eller flera ord ingår i en fras mha en ordlista
+fras='den däkan pular med sin motorcykel'
+ordlista=['däka','färas','pula']
 if any(word in fras for word in ordlista):
   print('Du är verkligen en äkta bleking!')
 else:
